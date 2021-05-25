fa6cc84d31b95b9c39c381ef029ad20fbd1465c5
Ramez <rameznoshy@gmail.com>
2021-05-25 21:03:41 +0200

    feat: needleman calculates alignments

--- src/algorithms/needleman.js
+++ src/algorithms/needleman.js
@@ -1,3 +1,36 @@
+const findAlignmentPaths = (cells) => {
+    const paths = [];
+    let currentPathIndex = 0;
+    paths.push([cells[cells.length - 1][cells[0].length - 1]]);
+    while (!allPathsFinished(paths)) {
+        const currentPath = paths[currentPathIndex];
+        const node = currentPath[currentPath.length - 1];
+        if (node.parents.length === 0) {
+            currentPathIndex = (currentPathIndex + 1) % paths.length;
+        } else if (node.parents.length === 1) {
+            paths[currentPathIndex].push(node.parents[0]);
+        } else {
+            for (let i = 0; i < node.parents.length; i++) {
+                const newPath = [...paths[currentPathIndex]];
+                newPath.push(node.parents[i]);
+                paths.push(newPath);
+            }
+            paths.splice(currentPathIndex, 1);
+        }
+    }
+    return paths;
+};
+
+const allPathsFinished = (paths) => {
+    let allFinished = true;
+    for (const path of paths) {
+        if (path[path.length - 1].parents.length !== 0) {
+            allFinished = false;
+        }
+    }
+    return allFinished;
+};
+
 const getNeedlemanMatrixCells = (string1, string2, matchScore, mismatchScore, penalty) => {
     const cells = [];
     console.log(string1);
@@ -7,33 +40,81 @@ const getNeedlemanMatrixCells = (string1, string2, matchScore, mismatchScore, pe
         for (let j = 0; j < string2.length + 1; j++) {
             const isMatch = string1[i - 1] === string2[j - 1];
             let newScore;
+            const parents = [];
             if (i === 0 && j === 0) {
                 newScore = 0;
             } else if (j !== 0 && i === 0) {
-                newScore = row[j - 1] + penalty;
+                newScore = row[j - 1].score + penalty;
+                parents.push(row[j - 1]);
             } else if (i !== 0 && j === 0) {
-                newScore = cells[i - 1][j] + penalty;
+                newScore = cells[i - 1][j].score + penalty;
+                parents.push(cells[i - 1][j]);
             } else {
                 const prevData = {
-                    row: row[j - 1],
-                    col: cells[i - 1][j],
-                    diagonal: cells[i - 1][j - 1],
+                    row: row[j - 1].score,
+                    col: cells[i - 1][j].score,
+                    diagonal: cells[i - 1][j - 1].score,
                 };
-                newScore = Math.max(prevData.row + penalty, prevData.col + penalty);
+                const rowParentScore = prevData.row + penalty;
+                const colParentScore = prevData.col + penalty;
+                newScore = Math.max(rowParentScore, colParentScore);
+                let diagParentScore;
                 if (isMatch) {
-                    console.log('true', i, string1[i - 1], j, string2[j - 1]);
-                    newScore = Math.max(newScore, prevData.diagonal + matchScore);
+                    diagParentScore = prevData.diagonal + matchScore;
                 } else {
-                    console.log('sad', i, string1[i - 1], j, string2[j - 1]);
-                    newScore = Math.max(newScore, prevData.diagonal + mismatchScore);
+                    diagParentScore = prevData.diagonal + matchScore + mismatchScore;
+                }
+                newScore = Math.max(newScore, diagParentScore);
+
+                if (newScore === rowParentScore) {
+                    parents.push(row[j - 1]);
+                }
+                if (newScore === colParentScore) {
+                    parents.push(cells[i - 1][j]);
+                }
+                if (newScore === diagParentScore) {
+                    parents.push(cells[i - 1][j - 1]);
                 }
             }
 
-            row.push(newScore);
+            row.push({ score: newScore, parents: parents, rowIndex: i, colIndex: j });
         }
         cells.push(row);
     }
-    return cells;
+
+    console.log('paths');
+    const paths = findAlignmentPaths(cells);
+    const alignments = [];
+    for (const reveresedPath of paths) {
+        const path = reveresedPath.reverse();
+        let alignedString1 = '';
+        let alignedString2 = '';
+        let string1Index = 0;
+        let string2Index = 0;
+        for (let i = 0; i < path.length - 1; i++) {
+            const node = path[i];
+            const nextNode = path[i + 1];
+            if (node.rowIndex === nextNode.rowIndex) {
+                alignedString1 = alignedString1 + '_';
+                alignedString2 = alignedString2 + string2[string2Index];
+                string2Index++;
+            } else if (node.colIndex === nextNode.colIndex) {
+                alignedString1 = alignedString1 + string1[string1Index];
+                string1Index++;
+                alignedString2 = alignedString2 + '_';
+            } else {
+                alignedString1 = alignedString1 + string1[string1Index];
+                alignedString2 = alignedString2 + string2[string2Index];
+                string1Index++;
+                string2Index++;
+            }
+        }
+        console.log(path);
+        console.log([alignedString1, alignedString2]);
+        alignments.push([alignedString1, alignedString2]);
+    }
+
+    return { cells: cells, alignments: alignments };
 };
 
 export default getNeedlemanMatrixCells;
